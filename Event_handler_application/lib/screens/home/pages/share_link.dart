323e7ba2b7dc9377cb16d8aab5221f3df865b1e0
deep <pardeep.kumar@studenti.unipd.it>
2022-01-03 19:52:46 +0100

    Added the button to share an event and tthe page to search for an event
    Added the button to share an event and tthe page to search for an event

--- Event_handler_application/lib/screens/home/pages/share_link.dart
+++ Event_handler_application/lib/screens/home/pages/share_link.dart
@@ -1,8 +1,18 @@
+import 'package:event_handler/models/event.dart';
 import 'package:event_handler/screens/events/event_screen.dart';
 import 'package:event_handler/screens/wrapper.dart';
+import 'package:event_handler/services/auth.dart';
+import 'package:event_handler/services/database.dart';
 import 'package:flutter/material.dart';
 
-class Share_Link extends StatelessWidget {
+class Share_Link extends StatefulWidget {
+  Share_Link({Key? key}) : super(key: key);
+  _ShareLinkState createState() => _ShareLinkState();
+}
+  class _ShareLinkState extends State<Share_Link> {
+  final AuthService _authService = AuthService();
+  String eventId='';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,17 +21,40 @@ class Share_Link extends StatelessWidget {
         body: Container(
           height: MediaQuery.of(context).size.height,
           color: Colors.white,
-          child: Center(
-            child: ElevatedButton( 
-            child: Text('Go to event'),
-            onPressed: ()async {
-               Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Wrapper()),//EventScreen(event: event,)),
-                  );
-                }
-            )
-          ),
+          child: ListView(
+            padding: EdgeInsets.only(top: 250, left: 25, right: 25),
+            children: [
+                TextFormField(
+                decoration: InputDecoration(labelText: 'Event Link'),
+                validator: (String? value) {
+                  if (value!.isEmpty) {
+                    return 'Event Link is required';
+                  }
+                },
+                onChanged: (value) {
+                  setState(() {
+                    eventId = value.trim();
+                  });
+                },
+              ),
+              SizedBox(height: 20),
+              FutureBuilder(
+                future: DatabaseService(_authService.getCurrentUser()!.uid).getEventByid(eventId),
+                initialData: "Loading text..",
+                builder: (BuildContext context, AsyncSnapshot<Object> event){
+                return ElevatedButton( 
+                    child: Text('Go to event'),
+                    onPressed: ()async {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => EventScreen(event: event.data as Event,)),
+                      );
+                    }
+                );
+              }
+
+              ),
+            ]),
         ),
       ),
     );
